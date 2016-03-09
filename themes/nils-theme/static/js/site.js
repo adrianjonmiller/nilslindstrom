cb4f0cae4f3d6192a0804fc62e5536cea49da01c
Adrian Miller <adrianjonmiller@gmail.com>
2016-03-09 13:40:08 -0800

    added new carosel

--- themes/nils-theme/static/js/site.js
+++ themes/nils-theme/static/js/site.js
@@ -1,43 +1,51 @@
 jQuery( document ).ready( function( $ ) {
 
   Js.Behaviors.scrollbar = function(container) {
-    var $this = $(container);
-    var $container = $this.find('.js-slider');
-    var offset = 0;
-    var itemsWidth = 0;
-
-    $(window).on('load', function(){
-      $.each($container.children(), function(index, item){
-        itemsWidth = itemsWidth + $(item).width();
-      });
-
-      $this.on('click', function(e){
-        var target = e.target;
-        console.log(itemsWidth);
-
-        if($(e.target).hasClass('js-left')) {
-          if(offset > 0) {
-            $container.animate(
-              {
-                scrollLeft: offset - $container.width()
-              },
-            500);
-            offset = offset - $container.width();
-          }
-        }
-
-        if($(e.target).hasClass('js-right')) {
-          if(offset < itemsWidth) {
-            $container.animate(
-              {
-                scrollLeft: offset + $container.width()
-              },
-            500);
-            offset = offset + $container.width();
-          }
-        }
-      });
+    $(container).slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true
     });
+    // var $this = $(container);
+    // var $container = $this.find('.js-slider');
+    // var offset = 0;
+    // var itemsWidth = 0;
+    //
+    // $(window).on('load', function(){
+    //   $.each($container.children(), function(index, item){
+    //     itemsWidth = itemsWidth + $(item).width();
+    //   });
+    //
+    //   $this.on('click', function(e){
+    //     var target = e.target;
+    //     console.log(itemsWidth);
+    //
+    //     if($(e.target).hasClass('js-left')) {
+    //       if(offset > 0) {
+    //         $container.animate(
+    //           {
+    //             scrollLeft: offset - $container.width()
+    //           },
+    //         500);
+    //         offset = offset - $container.width();
+    //       }
+    //     }
+    //
+    //     if($(e.target).hasClass('js-right')) {
+    //       if(offset < itemsWidth) {
+    //         $container.animate(
+    //           {
+    //             scrollLeft: offset + $container.width()
+    //           },
+    //         500);
+    //         offset = offset + $container.width();
+    //       }
+    //     }
+    //   });
+    // });
   }
 
   Js.Behaviors.gallery = function(container) {
